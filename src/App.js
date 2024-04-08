844f3efa82d6c2b17957976e3ca8a04a6974d99f
angiecao <caoangelina@gmail.com>
2024-04-08 02:44:25 -0400

    restored app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from 'react';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
 import Home from './components/pages/Home';
-//import Navbar from './components/Navbar';
+
+console.log(process.env.REACT_APP_MY_SUPABASE_URL);
+const supabase = createClient(process.env.REACT_APP_MY_SUPABASE_URL, process.env.REACT_APP_MY_SUPABASE_KEY)
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Navbar /> */}
-      <Home />
-    </div>
-  );
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+  })
+
+  const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+  })
+
+  return () => subscription.unsubscribe()
+  }, [])
+
+  console.log(session);
+
+  if (!session) {
+    return (
+      <div style={{ maxWidth: '400px', margin: "30px  " }}>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        <Home/>
+        HI! You are logged in! Your user ID is {session.user.id}. I can now send this to the backend to do user logic.
+        <button onClick={async (e) => {
+          supabase.auth.signOut()
+        }}>
+          SIGN OUT
+        </button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
