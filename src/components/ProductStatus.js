97e5b50bacdc4251e3c8d9836a0689c5d336e9c4
David Lee <seojindavid@gmail.com>
2024-04-27 14:00:36 -0400

    added pretty buttons and can buy from category now

--- src/components/ProductStatus.js
+++ src/components/ProductStatus.js
@@ -6,14 +6,26 @@ function ProductStatus({ status }) {
   return (
     <div className="product-status" style={{ backgroundColor: backgroundColor }}>
       <div className="status-text">{status}</div>
-      <img
+
+
+
+    </div>
+  );
+}
+
+
+
+/* 
+<img
         src={status === "Sold" ? "https://cdn.builder.io/api/v1/image/assets/TEMP/a024fbed528f73aa602bf9ae65005c45032408554b2b006eb0db5d3d40d82c31?apiKey=b8d09a4545bb49a8a3d7500b55db7534&" : "https://cdn.builder.io/api/v1/image/assets/TEMP/a024fbed528f73aa602bf9ae65005c45032408554b2b006eb0db5d3d40d82c31?apiKey=b8d09a4545bb49a8a3d7500b55db7534&"}
         alt=""
         className="status-icon"
       />
-    </div>
-  );
-}
+      */
+
+
+
+
 
 export default ProductStatus;
 
