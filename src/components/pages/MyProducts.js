2d1a3a5161b3712d7fc170a9058c3d260d62f499
David Lee <seojindavid@gmail.com>
2024-04-28 02:21:49 -0400

    fixed my products thing

--- src/components/pages/MyProducts.js
+++ src/components/pages/MyProducts.js
@@ -114,10 +114,10 @@ function MyProducts(props) {
               <h3>Product Name</h3>
               <h3>Product Status</h3>
             </div>
-            {prods.map(prods => (
-              <div key={prods.iid} className="product-row">
-                <ProductCard prods={prods} />
-                <ProductStatus status={"selling"} />
+            {prods.map((prod, index) => (
+              <div key={index} className="product-row">
+                <ProductCard prods={prod} handleBuy={handleBuy}/>
+                <ProductStatus status={prod.buyer ? "Sold" : "Selling"} />
               </div>
             ))}
           </div>
